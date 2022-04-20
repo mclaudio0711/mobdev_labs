02f985298e62af7e91c6087cd43e4f6f6ff3d5e6
MarceloClaudio <mclaudio.ccyber@hotmail.com>
2022-04-20 12:31:06 +0000

    week04

---
+++ mobdev-app/_oldwk4/src/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+    myVariable: string = 'The force is with me!!!!!'
+
+    updateMyValue() {
+     this.myVariable = 'Now the force is even stronger';
+     
+     }
+     
+      updateMyValue2() {
+        this.myVariable = 'Now the force is more even stronger!';
+      
+}
+}
+
